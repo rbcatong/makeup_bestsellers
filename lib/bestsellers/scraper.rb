76cb3fce916554721f48100f00034a2a39a33c0e
Rebecca Tong <rtong@buffalo.edu>
2018-01-30 19:06:33 +0000

    edited the scraper class to be able to hold the categories of the bestsellers

--- lib/bestsellers/scraper.rb
+++ lib/bestsellers/scraper.rb
@@ -4,9 +4,14 @@ require 'pry'
 
 class Scraper
 
+#create a category for each link - makeupbestsellers, skincare best sellers. 
   def scrape_website
+    categories = []
     page = Nokogiri::HTML(open("https://www.sephora.com/beauty-best-sellers?mediaId=14300062"))
-    binding.pry
+    page.css(".SideNav-link").each do |category|
+      categories << category.text
+      binding.pry
+    end
 
   end
 
