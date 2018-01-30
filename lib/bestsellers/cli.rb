5905dcdc3e6e158cc136fadc41de7987f322f2b6
Rebecca Tong <rtong@buffalo.edu>
2018-01-30 18:32:33 +0000

    added call method in cli and edited scraper method

--- lib/bestsellers/cli.rb
+++ lib/bestsellers/cli.rb
@@ -2,6 +2,17 @@ class Bestsellers::CLI
   attr_accessor :name, :price, :description
   
   def call
-    puts "welcome"
+    puts "Welcome to Sephora"
+    puts "Would you like to take a look at today's bestsellers?(Y/N)"
+    input = gets.strip
+    case(input)
+    when "Y"
+      list_details
+    when "N"
+      puts "Thank you for visiting, see you again soon!"
+    end
+  end
+  
+  def list_details
   end
 end
--- lib/bestsellers/scraper.rb
+++ lib/bestsellers/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Scraper
 
   def scrape_website
-    page = Nokogiri::HTML(open("https://www.sephora.com/"))
+    page = Nokogiri::HTML(open("https://www.sephora.com/beauty-best-sellers?mediaId=14300062"))
     binding.pry
 
   end
