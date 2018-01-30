b7739e5b202a37fd24d9761e8da6a74cff748e78
Rebecca Tong <rtong@buffalo.edu>
2018-01-30 20:08:47 +0000

    making small changes to require relatives, but ended up keeping it the same

--- lib/bestsellers/scraper.rb
+++ lib/bestsellers/scraper.rb
@@ -6,24 +6,23 @@ class Scraper
 
 #create a category for each link - makeupbestsellers, skincare best sellers. 
 #then assign a number for each one so that tehy can choose. 
-  # def list_of_categories
-  #   list_of_categories = []
-  #   page = Nokogiri::HTML(open("https://www.sephora.com/beauty-best-sellers?mediaId=14300062"))
-  #     page.css(".SideNav-link").each do |category|
-  #       list_of_categories << category.text.strip
-  #     end
-  # end
-
-    def product_name
-      page = Nokogiri::HTML(open("https://www.sephora.com/best-selling-makeup"))
-      page.css(".SkuItem-info").each do |item|
+  def list_of_categories
+    list_of_categories = []
+    page = Nokogiri::HTML(open("https://www.sephora.com/beauty-best-sellers?mediaId=14300062"))
+      page.css(".SideNav-link").each do |category|
+        list_of_categories << category.text.strip
         binding.pry
       end
-      
+  end
 
-    end
+    # def product_name
+    #   page = Nokogiri::HTML(open("https://www.sephora.com/best-selling-makeup"))
+    #   page.css(".SkuItem-info").each do |item|
+    #     binding.pry
+    #   end
+      #end
 
 
 end
 
-Scraper.new().product_name
+Scraper.new().list_of_categories
