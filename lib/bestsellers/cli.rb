2394dd6825ca91e8aa9b5c963c9bfa0f59d42a68
Rebecca Tong <rtong@buffalo.edu>
2018-02-05 03:50:01 +0000

    created listproductcategories method for the categories and fixed start method

--- lib/bestsellers/cli.rb
+++ lib/bestsellers/cli.rb
@@ -1,11 +1,16 @@
 class Bestsellers::CLI
 
-  @scraper = Scraper.new
+  #@scraper = Scraper.new
 
   def start
     puts "Welcome to Sephora"
     puts "Would you like to take a look at today's bestsellers?(Y/N)"
-    list_product_categories
+    input = gets.strip
+    case(input)
+    when "Y"
+      list_product_categories
+    when "N"
+      puts "Thank you for visiting, see you again soon!"
       # #while input != "N"
       #   puts "Would you like to take a look at today's bestsellers?"
       # case(input)
@@ -17,10 +22,15 @@ class Bestsellers::CLI
   end
 
   def list_product_categories
-    puts "Which category would you like to see?"
+    puts "Which category would you like to see? Type the number of the category that you'd like to see."
+    puts "1) Makeup"
+    puts "2) Skincare"
+    puts "3) Fragrance"
+    puts "4) Bath&Body"
+    puts "5) Hair"
     #grabs the method from scraper
-    scraper = Scraper.new
-    scraper.list_of_categories
+    #scraper = Scraper.new
+    #scraper.list_of_categories
   end
 
 
