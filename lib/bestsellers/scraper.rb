a0a2f800a9dc617e612af47a8f617df9761b4348
Rebecca Tong <rtong@buffalo.edu>
2018-02-07 02:58:27 +0000

    added skincare scraping method and makeup skincare scraping method

--- lib/bestsellers/scraper.rb
+++ lib/bestsellers/scraper.rb
@@ -13,14 +13,22 @@ class Scraper
   #   #create strings from array...
   # end
 
-  def product_name
+  def makeup_product_name
+    makeup = []
     page = Nokogiri::HTML(open("https://www.ulta.com/makeup-allure-best-of-beauty?N=1g7h4q6"))
-    page.css(".prod-title").each do |item|
+    page.css(".prod-desc").each do |item|
+      makeup << item.text.strip
+    end
+  end #do a list of hashes with price, makeup brand decscription.. if they want to know more we can give link.
+
+  def skincare_product_name
+    skincare = []
+    page = Nokogiri::HTML(open("https://www.ulta.com/skin-care-allure-best-of-beauty?N=yrcmiw"))
+    page.css(".prod-desc").each do |item|
       binding.pry
+      skincare << item.text.strip
     end
   end
-
-
 end
 
-Scraper.new().product_name
+Scraper.new().skincare_product_name
