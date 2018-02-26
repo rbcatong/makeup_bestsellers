9f2a207062dc6bb9f699f992853e4a45c7894834
Rebecca Tong <rtong@buffalo.edu>
2018-02-26 01:48:58 +0000

    adding a makeup_menu method

--- lib/bestsellers/cli.rb
+++ lib/bestsellers/cli.rb
@@ -24,12 +24,13 @@ class Bestsellers::CLI
 
   def list_product_categories
     puts "Which category would you like to see? Type the number of the category that you'd like to see or 3 to exit out."
-    puts "1) Makeup -- Press a number if you'd like more information"
-    puts "2) Skincare -- Press a number if you'd like more information"
+    puts "1) Makeup"
+    puts "2) Skincare"
     puts "3) Exit"
     input = gets.strip
     case(input)
     when "1"
+      makeup_menu
       show_makeup_info
     when "2"
       show_skincare_info
@@ -51,35 +52,49 @@ class Bestsellers::CLI
     display_makeup_url
   end
 
+  def makeup_menu
+    puts "What item would you like to see? Type in the number that you would like to see."
+    input = gets.strip
+    case(input)
+    when "Main Menu"
+      list_product_categories
+    when "Exit"
+      puts "Goodbye"
+      exit
+    else puts "Please try again."
+      makeup_menu
+    end
+  end
+
 
   def display_makeup_url
     input = gets.strip
     collection_of_makeup = Bestsellers::Scraper.scrape_top_makeup
-      if input.to_i <= collection_of_makeup.length
-        puts "Url: https://www.ulta.com/#{collection_of_makeup[input.to_i-1].url}"
-      elsif
-        show_makeup_info
-      end
+    if input.to_i <= collection_of_makeup.length
+      puts "Url: https://www.ulta.com/#{collection_of_makeup[input.to_i-1].url}"
+    elsif
+      show_makeup_info
     end
-
-
-    def show_skincare_info
-      Bestsellers::Scraper.scrape_top_skincare.each.with_index(1) do |item, i|
-        puts "#{i}. Name: #{item.name}
-        Description: #{item.description}
-        Price: #{item.price} "
-      end
-      display_skincare_url
-    end
-
-    def display_skincare_url
-      input = gets.strip
-      collection_of_skincare = Bestsellers::Scraper.scrape_top_skincare
-        if input.to_i <= collection_of_skincare.length
-          puts "Url: https://www.ulta.com/#{collection_of_skincare[input.to_i-1].url}"
-        elsif
-          show_skincare_info
-        end
-    end
-
   end
+
+
+  def show_skincare_info
+    Bestsellers::Scraper.scrape_top_skincare.each.with_index(1) do |item, i|
+      puts "#{i}. Name: #{item.name}
+      Description: #{item.description}
+      Price: #{item.price} "
+    end
+    display_skincare_url
+  end
+
+  def display_skincare_url
+    input = gets.strip
+    collection_of_skincare = Bestsellers::Scraper.scrape_top_skincare
+    if input.to_i <= collection_of_skincare.length
+      puts "Url: https://www.ulta.com/#{collection_of_skincare[input.to_i-1].url}"
+    elsif
+      show_skincare_info
+    end
+  end
+
+end
