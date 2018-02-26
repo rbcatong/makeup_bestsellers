58a3c2bdec1a16502b1bc9fe86cf33b7331f2c95
Rebecca Tong <rtong@buffalo.edu>
2018-02-26 18:43:35 +0000

    cleaned up method display_skincare_url and display_makeup_url

--- lib/bestsellers/cli.rb
+++ lib/bestsellers/cli.rb
@@ -65,11 +65,11 @@ class Bestsellers::CLI
   end
 
   def display_makeup_url
-    input = gets.strip
+    input = gets.strip.to_i
     collection_of_makeup = Bestsellers::Scraper.scrape_top_makeup
-    if input.to_i <= collection_of_makeup.length
-      puts "Url: https://www.ulta.com/#{collection_of_makeup[input.to_i-1].url}"
-    elsif
+    if input <= collection_of_makeup.length
+      puts "Url: https://www.ulta.com/#{collection_of_makeup[input-1].url}"
+    else
       puts "Please try again"
       show_makeup_info
     end
@@ -99,11 +99,11 @@ class Bestsellers::CLI
   end
 
   def display_skincare_url
-    input = gets.strip
+    input = gets.strip.to_i
     collection_of_skincare = Bestsellers::Scraper.scrape_top_skincare
-    if input.to_i <= collection_of_skincare.length
-      puts "Url: https://www.ulta.com/#{collection_of_skincare[input.to_i-1].url}"
-    elsif
+    if input <= collection_of_skincare.length
+      puts "Url: https://www.ulta.com/#{collection_of_skincare[input-1].url}"
+    else
       puts "Please try again."
       show_skincare_info
     end
